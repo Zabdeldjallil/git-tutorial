ee5a87bbf938475a417e556d4900f1ef656a219d
djilou <djillou500@gmail.com>
2022-05-30 21:05:44 +0100

    reset state

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(10)
+  const [count, setCount] = useState(80)
 
   return (
     <div className="App">
